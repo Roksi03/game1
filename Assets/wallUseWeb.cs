56b78b750dae4670e28b292b575f7d437c36dc06
xanstered <gzak2@edu.cdv.pl>
2024-05-07 16:40:46 +0200

    webbar

--- Assets/wallUseWeb.cs
+++ Assets/wallUseWeb.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Windows.WebCam;
 public class wallUseWeb : MonoBehaviour
 {
     public float web, maxWeb = 100;
 
+    float lerpSpeed;
+
+    public Image webBar;
+
     public TextMeshProUGUI webText;
+
+    private void Update()
+    {
+        lerpSpeed = 3f * Time.deltaTime;
+
+        WebBarFiller();
+        ColorChanger();
+    }
+    void WebBarFiller()
+    {
+        webBar.fillAmount = Mathf.Lerp(webBar.fillAmount, web / maxWeb, lerpSpeed);
+    }
+
+    void ColorChanger()
+    {
+        Color webColor = Color.Lerp(Color.red, Color.white, (web / maxWeb));
+
+        webBar.color = webColor;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
