b13cd52afe8663cf28c28ef219f232855ffe1b6f
xanstered <gzak2@edu.cdv.pl>
2024-05-22 22:47:54 +0200

    mucha

--- Assets/USED SCRIPTS/webChange.cs
+++ Assets/USED SCRIPTS/webChange.cs
@@ -6,10 +6,33 @@ public class webChange : MonoBehaviour
 {
     [SerializeField] private GameObject webChanger;
     [SerializeField] private GameObject webChangerTarget;
+    public Animator animator; // The Animator component attached to the player sprite
+    public string animationTriggerName = "flyCatch"; // The name of the trigger parameter in the Animator
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Instantiate(webChanger, transform.position, Quaternion.identity);
-        Destroy(webChangerTarget);
     }
+
+    void Start()
+    {
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        if (animator == null)
+        {
+            Debug.LogError("Animator component not found on the GameObject.");
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "throwWeb")
+        {
+            animator.SetTrigger("flyCatch");
+        }
+    }
+
 }
